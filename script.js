f12c7252b86cf774e47d152a809a313d07bd4f15
Sourav Sharma <souravsharma3629@gmail.com>
2025-09-07 00:39:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ tilt.addEventListener('mouseleave', () => {
   tilt.style.transform = 'rotateX(0) rotateY(0)';
 });
 
+
 // Form submission (demo)
 const form = document.getElementById('contact-form');
 const status = document.getElementById('form-status');
@@ -40,6 +41,7 @@ toggleBtn.addEventListener('click', () => {
 
 
 // Listen for form submission
+
 
 
 
